deb6878bf62321d1a3e8f5716eaaf63b2db3d08b
nouhailaNM <62950704+nouhailaNM@users.noreply.github.com>
2020-06-01 09:25:52 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDTrSgi7BLYZFJaO3xLvjW142RupQxcjUg",
+    authDomain: "covid-19-mo-3e643.firebaseapp.com",
+    databaseURL: "https://covid-19-mo-3e643.firebaseio.com",
+    projectId: "covid-19-mo-3e643",
+    storageBucket: "covid-19-mo-3e643.appspot.com",
+    messagingSenderId: "381165784425",
+    appId: "1:381165784425:web:c67d05be1954b339a701f8",
+    measurementId: "G-QJGYJM8ZX1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
